7bc7558028cff248f6374b90c96422a1c1a6ef96
Nmargolis00 <119348315+Nmargolis00@users.noreply.github.com>
2023-01-18 09:59:24 -0700

    psuedo code in js

---
+++ script.js
@@ -0,0 +1,12 @@
+// Site that creates a joint calendar that pulls both partners work schedules and overlaps them so it can do scheduling assistant. It also allows for the input of personal events outside of work on a shared private calendar
+
+
+//Separate idea is a joint note app that will send email updates when the notes are updated. Is this easier?
+
+//Step 1 create a calendar with a user log in
+    // ihateregex for how to confirm that it is an email: https://www.abstractapi.com/guides/email-validation-regex-javascript#:~:text=Using%20Regular%20Expressions%20in%20Javascript%20Email%20Validation&text=You%20just%20need%20to%20create,%60%7B%7C%7D~%5E.
+    //this site shows how to to do it
+//Step 2 third party API to pull work meetings/events. Have this link with Outlook
+//Import into shared calendar and overlay
+//Scheduling assistant
+//When you schedule a meeting it will alert partner's email
